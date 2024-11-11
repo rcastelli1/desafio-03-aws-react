fcf86def170d90142d9b713cd6eaff6cea5485bf
Ryan Castelli <ryancastelli19@gmail.com>
2024-11-11 13:46:53 -0300

    feat: add state for user data, edit mode, and form values

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,8 +1,18 @@
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const Portfolio = () => {
   const location = useLocation();
   const { login, isAuthenticatedUser } = location.state || {};
+
+  const [userData, setUserData] = useState(null);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [formValues, setFormValues] = useState({
+    name: localStorage.getItem("name") || "Fulano",
+    linkedin: localStorage.getItem("linkedin") || "",
+    bio: localStorage.getItem("bio") || "",
+    email: localStorage.getItem("email") || "",
+  });
   
   return (
     <div></div>
